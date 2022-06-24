c13f712c7a66ff997193c54fd96c0d0aee4d242f
salsabilamaaa <salsabilama4@gmail.com>
2022-06-25 00:11:59 +0700

    cinta abp

--- lib/models/Resto.dart
+++ lib/models/Resto.dart
@@ -7,34 +7,35 @@ import 'package:tubes_resto/models/Menu.dart';
 // String pesananToJson(List<Resto> data)=>json.encode(List<dynamic>.from(data.map((x)=>x.toJson())));
 
 class Resto {
-  late int idResto;
-  late String nameResto,
-      username,
-      open,
-      image,
-      alamat,
-      kota,
-      kecamatan,
-      kodepos;
-  late int minPeople, maxPeople;
-  late List<Menu> listMenu;
+  final int idResto;
+  final String nameResto, username, image, alamat, kota, kecamatan, kodepos;
+  // String open = "09.00 - 22.00";
+  // int minPeople, maxPeople;
+  // late List<Menu> listMenu;
+  final String category;
 
-  Resto(this.idResto, this.nameResto, this.open, this.minPeople, this.maxPeople,
-      this.listMenu);
+  const Resto(
+      {required this.idResto,
+      required this.nameResto,
+      required this.username,
+      required this.image,
+      required this.alamat,
+      required this.kota,
+      required this.kecamatan,
+      required this.kodepos,
+      required this.category});
+  // static fromJson(json) {}
 
-  static fromJson(json) {}
-
-   factory Resto.fromJson(Map<String, dynamic> json) => Resto (
-      idResto: json['resto_id'],
-      nameResto: json['resto_id'],
-      username: json['username'],
-      image: json['image'] as String,
-      alamat: json['alamat'],
-      kota: json['kota'],
-      kecamatan: json['kecamatan'],
-      kodepos: json['username'],
-      minPeople: json['username'],
-      maxPeople: json['username'],
-      listMenu: json['username'],
-    );
+  factory Resto.fromJson(Map<String, dynamic> json) {
+    return Resto(
+        idResto: json['id'] as int,
+        nameResto: json['nama'] as String,
+        username: json['username'] as String,
+        image: json['image'] as String,
+        alamat: json['alamat'] as String,
+        kota: json['kota'] as String,
+        kecamatan: json['kecamatan'] as String,
+        kodepos: json['username'] as String,
+        category: json['kategori'] as String);
+  }
 }
